60fc6ce5a6859ab39481fb16329e871288212bd4
beejay321 <jibodu.busola@gmail.com>
2021-06-16 16:57:36 +0200

    post experience route

--- src/Components/CardProfile.jsx
+++ src/Components/CardProfile.jsx
@@ -1,71 +1,66 @@
-import { Card, Button } from "react-bootstrap"
-import "../CardProfile.css"
-import ModalForm from "./MyModal"
-import React from "react"
+import { Card, Button } from "react-bootstrap";
+import "../CardProfile.css";
+import ModalForm from "./MyModal";
+import React from "react";
 
 class CardProfile extends React.Component {
   state = {
     experience: {},
-  }
+  };
 
   handleSubmit = async (e) => {
-    e.preventDefault()
-    console.log("ciaociao")
+    e.preventDefault();
+    console.log("ciaociao");
     try {
-      const response = await fetch(
-        `https://api-linkedin-api.herokuapp.com/profile/${this.props.user}/experiences`,
-        {
-          method: "POST",
-          headers: {
-            
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(this.state.experience),
-        }
-      )
+      const response = await fetch(`https://api-linkedin-api.herokuapp.com/profile/60c9be8b6f63455fa0ee7849}/experiences`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.state.experience),
+      });
       if (response.ok) {
-        const data = await response.json()
-        console.log(data)
+        const data = await response.json();
+        console.log(data);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   handleChange = (e) => {
-    let id = e.target.id
+    let id = e.target.id;
     this.setState({
       experience: { ...this.state.experience, [id]: e.target.value },
-    })
-  }
+    });
+  };
 
   render() {
-    console.log(this.props.user)
+    console.log(this.props.user);
     return (
       <Card className="my-3">
         <Card.Body>
           <div className=" d-flex justify-content-between">
             <div className="section-title mb-3">{this.props.title}</div>
             <div>
-              {this.props.title === "Experience" &&
-                this.props.isMe === "me" && (
-                  <ModalForm
-                    handleSubmit={this.handleSubmit}
-                    handleChange={this.handleChange}
-                    role={this.state.experience.role}
-                    company={this.state.experience.company}
-                    startDate={this.state.experience.startDate}
-                    description={this.state.experience.description}
-                    area={this.state.experience.area}
-                  />
-                )}
+              {this.props.title === "Experience" && this.props.isMe === "me" && (
+                <ModalForm
+                  handleSubmit={this.handleSubmit}
+                  handleChange={this.handleChange}
+                  role={this.state.experience.role}
+                  company={this.state.experience.company}
+                  startDate={this.state.experience.startDate}
+                  description={this.state.experience.description}
+                  area={this.state.experience.area}
+                />
+              )}
             </div>
           </div>
           <div className="text-size">{this.props.content}</div>
         </Card.Body>
       </Card>
-    )
+    );
   }
 }
 
-export default CardProfile
+export default CardProfile;
