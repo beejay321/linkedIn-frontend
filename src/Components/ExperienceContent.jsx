804c0ec96b41dad55cbed55c0807a2873ba59b0d
beejay321 <jibodu.busola@gmail.com>
2021-06-16 16:47:00 +0200

    Update exps

--- src/Components/ExperienceContent.jsx
+++ src/Components/ExperienceContent.jsx
@@ -1,30 +1,27 @@
-import ExpEduCard from "./ExpEduCard"
+import ExpEduCard from "./ExpEduCard";
 
 const ExperienceContent = (props) => {
-  const experiences = props.experiences
+  const experience = props.experiences;
+  // const experiencess = experiences.experiences
+  console.log("this is the shitty experience", experience);
   return (
     <>
-      {experiences.map((exp) => {
-        return (
-          <ExpEduCard
-            experience={exp}
-            userId={props.userId}
-            userName={props.userName}
-            jobOrSchool={exp.role}
-            companyOrSubject={exp.company}
-            duration={exp.startDate}
-            location={exp.area}
-            description={exp.description}
-            id={exp._id}
-            img={
-              exp.image ||
-              "https://res.cloudinary.com/dmqsfltrf/image/upload/v1607933865/linkedin/d5ncpqvqrjwdxixjuyjr.ico"
-            }
-          />
-        )
-      })}
+      {experience.map((exp) => (
+        <ExpEduCard
+          experience={exp}
+          userId={props.userId}
+          userName={props.userName}
+          jobOrSchool={exp.role}
+          companyOrSubject={exp.company}
+          duration={exp.startDate}
+          location={exp.area}
+          description={exp.description}
+          id={exp._id}
+          img={exp.image || "https://res.cloudinary.com/dmqsfltrf/image/upload/v1607933865/linkedin/d5ncpqvqrjwdxixjuyjr.ico"}
+        />
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default ExperienceContent
+export default ExperienceContent;
